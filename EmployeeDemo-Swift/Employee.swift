1248583ab996ffc3987fb9167c53d1cdff36d614
Wayne Delport <Wayne.Delport@standardbank.co.za>
2018-12-19 07:17:41 +0200

    Self encapsulate Type field

--- EmployeeDemo-Swift/Employee.swift
+++ EmployeeDemo-Swift/Employee.swift
@@ -12,14 +12,18 @@ enum EmployeeType {
 }
 
 class Employee {
-    private let type: EmployeeType
+    private var type: EmployeeType!
 
     init(type: EmployeeType) {
+        setTypeCode(type: type)
+    }
+
+    private func setTypeCode(type: EmployeeType) {
         self.type = type
     }
 
     func payTotal() throws -> Int {
-        switch (type) {
+        switch getTypeCode() {
         case .engineer:
             return 1000
         case .salesman:
@@ -28,5 +32,9 @@ class Employee {
             return 100
         default: throw EmployeeError.unknownEmployee
         }
+    }
+
+    private func getTypeCode() -> EmployeeType {
+        return type
     }
 }
